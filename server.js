422a073be087a50ceeabaeeb03d84ff37ff92a24
Vaibhav Krishna U <vaibhavkrishna0607@gmail.com>
2025-02-11 14:24:01 +0530

    done

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello Kalvian");
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+app.post('/signup', (req, res) => {
+    const username = req.params.username;
+    if(!username){
+        res.send("Username cannot be empty");
+    }
+    const email = req.params.email;
+    if(!email){
+        res.send("Email cannot be empty");
+    }
+    
+    const password = req.params.password;
+    if(password.length<8 || password.length>=16){
+        res.send("Password length should be greater than 8 or less than or equal to 16");
+}});
+
