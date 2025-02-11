6f2724387dd1b5323db9920f852804a8f4e40a46
Vaibhav Krishna U <vaibhavkrishna0607@gmail.com>
2025-02-11 14:55:01 +0530

    testing

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello Kalvian");
-})
+// app.get('/', (req, res) => {
+//     res.send("Hello Kalvian");
+// })
 
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
-
-app.post('/signup', (req, res) => {
+app.post('/', (req, res) => {
     const username = req.params.username;
     if(!username){
         res.send("Username cannot be empty");
@@ -20,9 +14,12 @@ app.post('/signup', (req, res) => {
     if(!email){
         res.send("Email cannot be empty");
     }
-    
     const password = req.params.password;
     if(password.length<8 || password.length>=16){
         res.send("Password length should be greater than 8 or less than or equal to 16");
 }});
 
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
