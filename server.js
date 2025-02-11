2c7243280763642729e47817ec3b708f0a92459a
Vaibhav Krishna U <vaibhavkrishna0607@gmail.com>
2025-02-11 16:48:13 +0530

    done

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send("Hello Kalvian");
-// })
+
+app.use(express.json());
 
 app.post('/', (req, res) => {
-    const username = req.params.username;
-    if(!username){
-        res.send("Username cannot be empty");
+    const { username, email, password } = req.body;
+
+    if (!username) {
+        return res.status(400).send("Username cannot be empty");
     }
-    const email = req.params.email;
-    if(!email){
-        res.send("Email cannot be empty");
+    if (!email) {
+        return res.status(400).send("Email cannot be empty");
     }
-    const password = req.params.password;
-    if(password.length < 8 || password.length >= 16){
-        res.send("Password length should be greater than 8 or less than or equal to 16");
-}});
+    if (password.length <= 8 || password.length > 16) {
+        return res.status(400).send("Password length should be greater than 8 and less than or equal to 16");
+    }
+
+    
+
+    res.send("Credentials saved successfully");
+});
 
 const port = 3000;
 app.listen(port, () => {
